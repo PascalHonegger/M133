72b6f12bd04d9fdc48dbfbc16c68dbe18547041b
Alain Keller <alain.keller@edu.tbz.ch>
2016-01-05 14:38:53 +0100

    ##2 started creating the view

---
+++ view/view.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="../css/styles.css">
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+    <div id="container">
+        <div id="header">
+            <div id="headerlogo">
+
+            </div>
+            <div id="navigation">
+                <ul id="navlist">
+                    <li>Main</li>
+                    <li>Moneywaste</li>
+                    <li>Monywastepreventor</li>
+                </ul>
+            </div>
+
+        </div>
+        <div id="maincontent">
+            <div id="menu">
+
+            </div>
+            <div id="contnet">
+
+            </div>
+        </div>
+        <div id="footer">
+
+        </div>
+
+    </div>
+</body>
+</html>

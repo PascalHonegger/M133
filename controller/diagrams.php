d189072eda3e992ccc32e898ff15bc3333eac0ae
Alain Keller <alain.keller@edu.tbz.ch>
2016-01-23 14:52:03 +0100

    changes in diagram

--- controller/diagrams.php
+++ controller/diagrams.php
@@ -156,8 +156,8 @@ $diverses = $diverses['count'];
     });
 });
 		</script>
-<script src="https://code.highcharts.com/highcharts.js"></script>
-<script src="https://code.highcharts.com/modules/exporting.js"></script>
+<script src="../diagrams/highcharts.js"></script>
+<script src="../diagrams/modules/exporting.js"></script>
 
 <div id="container" style="min-width: 310px; max-width: 800px; height: 400px; margin: 0 auto"></div>
 
